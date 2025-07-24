a7292e42dc6151b41fb0de97f912a4e7421b8af6
unknown <andresandrada1994@gmail.com>
2025-07-23 22:24:32 -0300

    Correcciones en el backend (por ejemplo, puerto, dependencias, JWT)

--- src/main/java/com/bitsion/apirest/apirest/Repositories/PersonRepository.java
+++ src/main/java/com/bitsion/apirest/apirest/Repositories/PersonRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.bitsion.apirest.apirest.Entities.Person;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
-    // boolean existsByIdentification(String identification);
+    boolean existsByDocumentation(String documentation);
 }
